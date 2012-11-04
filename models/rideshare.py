45cbb73bb857027efa91bcc8559d085bb22c1c52
Ryan Ho <rho1@ucsc.edu>
2012-11-04 01:47:48 -0700

    added some validators for adding a ride

--- models/rideshare.py
+++ models/rideshare.py
@@ -10,15 +10,16 @@ db.define_table('people',
                 Field('age', 'integer', requires = 'integer'))
                 
 db.define_table('ride',
-                Field('meeting_location', 'string'),
-                Field('destination', 'string'),
-                Field('departure_time', 'string'),
-                Field('trip_length', 'string'),
-                Field('completed', 'string'),
+                Field('meeting_location', 'string', requires=IS_NOT_EMPTY()),
+                Field('destination', 'string', requires=IS_NOT_EMPTY()),
+                Field('departure_time'),
+                Field('trip_length'),
+                Field('completed'),
                 Field('comments', 'text'),
-                Field('price', 'string'),
-                Field('number_of_seats_open'),
+                Field('price'),
+                Field('number_of_seats_open', requires=IS_IN_SET(['0', '1', '2','3','4','5'])),
                 Field('owner', db.auth_user, default=auth.user_id))
+                
                 
 db.ride.completed.writable = db.ride.completed.readable = False
 db.ride.owner.writable = db.ride.owner.readable = False
